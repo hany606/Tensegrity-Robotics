c0d2660c1c8bfc06e1b8a09950b5446e3c49d853
Hany Hamed <h.hamed.elanwar@gmail.com>
2019-09-28 20:16:35 +0300

    Add stable Tensegrity Leg v0

--- src/dev/legz/legzModel.cpp
+++ src/dev/legz/legzModel.cpp
@@ -54,15 +54,28 @@ namespace
         double stiffness;
         double damping;
         double pretension;
-        double legz_length;  
+        double legz_length;
+        bool hist;
+        double maxTension;
+
     } c =
    {
+    //    0.2,     // density (mass / length^3)
+    //    0.20,     // radius (length)
+    //    1000.0,   // stiffness (mass / sec^2)
+    //    20.0,     // damping (mass / sec)
+    //    12000.0,     // pretension (mass * length / sec^2)
+    //    20.0,     // legz_height (length)
+
        0.2,     // density (mass / length^3)
        0.20,     // radius (length)
        1000.0,   // stiffness (mass / sec^2)
        20.0,     // damping (mass / sec)
        12000.0,     // pretension (mass * length / sec^2)
-       20.0,     // legz_height (length)
+       20.0,        //length of bar
+       0,           // history logging (boolean)
+       30000,       // max tension
+
     // 0.2,     // density (mass / length^3)
     // 0.31,     // radius (length)
     // 1,   // stiffness (mass / sec^2)
@@ -95,38 +108,38 @@ void legzModel::addNodes(tgStructure& s, double length)
     // s.addNode( edge / 2.0, height, 0); // 5
     // // top front
     // s.addNode(0, height, width); // 6
-    s.addNode(0, length/2.0, -length/8.0);
-    s.addNode(0, -length/2.0, -length/8.0);
+    s.addNode(length/8.0, length/2.0, 0);
+    s.addNode(length/8.0, -length/2.0, 0);
     //2 = 1 but up same z,y 
-    s.addNode(0, length/2.0, length/8.0);
-    s.addNode(0, -length/2.0,  length/8.0);
+    s.addNode(-length/8.0, length/2.0, 0);
+    s.addNode(-length/8.0, -length/2.0,  0);
 
     //3 parallel to z-axis  in the middle of 1 & 2
     //for the x I am not sure 1.5
-    s.addNode(length*2, length/5.0, 0);
-    s.addNode(-length*2, length/5.0, 0);
+    s.addNode(0, length/5.0, length*2);
+    s.addNode(0, length/5.0, -length*2);
     //4 = 3 but right same y,z
-    s.addNode(length*2, -length/5.0, 0);
-    s.addNode(-length*2, -length/5.0, 0);
+    s.addNode(0, -length/5.0, length*2);
+    s.addNode(0, -length/5.0, -length*2);
     
     //5 parallel to y 8,3
-    s.addNode(length/8.0, 0, length*1.5);
-    s.addNode(length/8.0, 0, -length*1.5);
+    s.addNode(-length*1.5, 0, length/8.0);
+    s.addNode(length*1.5, 0, length/8.0);
     //6 = 5 but backward and smaller 8,1
-    s.addNode(-length/8.0, 0, length/2.0);
-    s.addNode(-length/8.0, 0, -length/2.0);
+    s.addNode(-length/2.0, 0, -length/8.0);
+    s.addNode(length/2.0, 0, -length/8.0);
 
 
     //7 = 6 but more backward 11/8,1
-    s.addNode(-11*length/8.0, 0, length/2.0);
-    s.addNode(-11*length/8.0, 0, -length/2.0);
+    s.addNode(-length/2.0, 0, -11*length/8.0);
+    s.addNode(length/2.0, 0, -11*length/8.0);
 
     //8 = 1 but backward 
-    s.addNode(-length, length/2.0, -length/8.0);
-    s.addNode(-length, -length/2.0, -length/8.0);
+    s.addNode(length/8.0, length/2.0, -length);
+    s.addNode(length/8.0, -length/2.0, -length);
 
-    s.addNode(-length, length/2.0, length/8.0);
-    s.addNode(-length, -length/2.0,  length/8.0);
+    s.addNode(-length/8.0, length/2.0, -length);
+    s.addNode(-length/8.0, -length/2.0,  -length);
 
 }
 
@@ -155,64 +168,102 @@ void legzModel::addMuscles(tgStructure& s)
     // s.addPair(1, 4,  "muscle");
     // s.addPair(2, 5,  "muscle");
 
-    s.addPair(0, 9,  "muscle");
-    // s.addPair(0, 5,  "muscle"); //#NO
-    s.addPair(0, 4,  "muscle");
-    s.addPair(0, 11,  "muscle");
-    s.addPair(1, 9,  "muscle");
-    s.addPair(1, 6,  "muscle");
+    // s.addPair(0, 9,  "muscle");
+    // // s.addPair(0, 5,  "muscle"); //#NO
+    // s.addPair(0, 4,  "muscle");
+    // s.addPair(0, 11,  "muscle");
+    // s.addPair(1, 9,  "muscle");
+    // s.addPair(1, 6,  "muscle");
 
-    s.addPair(1, 11,  "muscle");
+    // s.addPair(1, 11,  "muscle");
 
     
-    s.addPair(4, 9,  "muscle");
-    s.addPair(6, 9,  "muscle");
+    // s.addPair(4, 9,  "muscle");
+    // s.addPair(6, 9,  "muscle");
 
-    // s.addPair(1, 7,  "muscle"); //#NO
+    // // s.addPair(1, 7,  "muscle"); //#NO
+    // s.addPair(11, 7,  "muscle");
+    // s.addPair(11, 5,  "muscle");
+
+    // s.addPair(6, 3,  "muscle"); //#
+    // s.addPair(4, 3,  "muscle"); //#
+
+    // // s.addPair(7, 3,  "muscle"); //#NO
+    // // s.addPair(5, 3,  "muscle"); //#NO
+
+    // s.addPair(8, 6,  "muscle");
+    // s.addPair(8, 4,  "muscle");
+    // s.addPair(8, 2,  "muscle");
+    // s.addPair(8, 3,  "muscle");
+
+    // s.addPair(10, 2,  "muscle");
+    // s.addPair(10, 3,  "muscle");
+
+    // s.addPair(10, 5,  "muscle");
+    // s.addPair(10, 7,  "muscle");
+
+    // s.addPair(13, 5,  "muscle");
+    // s.addPair(13, 7,  "muscle");
+    // s.addPair(12, 5,  "muscle");
+    // s.addPair(12, 7,  "muscle");
+    // s.addPair(13, 15,  "muscle");
+    // s.addPair(13, 14,  "muscle");
+    // s.addPair(12, 17,  "muscle");
+    // s.addPair(12, 16,  "muscle");
+
+    // s.addPair(11, 14,  "muscle");
+    // s.addPair(11, 15,  "muscle");
+    // s.addPair(11, 17,  "muscle"); //#
+    // s.addPair(11, 16,  "muscle"); //#
+    // s.addPair(10, 16,  "muscle");
+    // s.addPair(10, 17,  "muscle");
+
+
+    // s.addPair(10, 15,  "muscle");   //#
+    // s.addPair(10, 14,  "muscle");   //#
+
+    // s.addPair(14, 5,  "muscle");
+    // s.addPair(15, 7,  "muscle");
+    // s.addPair(16, 5,  "muscle");
+    // s.addPair(17, 7,  "muscle");
+
+    s.addPair(1, 6,  "muscle");
+    s.addPair(1, 11,  "muscle");
+    s.addPair(1, 9,  "muscle");
+    s.addPair(0, 4,  "muscle");
+    s.addPair(0, 11,  "muscle");
+    s.addPair(0, 9,  "muscle");
+    s.addPair(9, 6,  "muscle");
+    s.addPair(9, 4,  "muscle");
     s.addPair(11, 7,  "muscle");
     s.addPair(11, 5,  "muscle");
-
-    s.addPair(6, 3,  "muscle"); //#
-    s.addPair(4, 3,  "muscle"); //#
-
-    // s.addPair(7, 3,  "muscle"); //#NO
-    // s.addPair(5, 3,  "muscle"); //#NO
-
-    s.addPair(8, 6,  "muscle");
-    s.addPair(8, 4,  "muscle");
-    s.addPair(8, 2,  "muscle");
-    s.addPair(8, 3,  "muscle");
-
-    s.addPair(10, 2,  "muscle");
-    s.addPair(10, 3,  "muscle");
-
-    s.addPair(10, 5,  "muscle");
-    s.addPair(10, 7,  "muscle");
-
-    s.addPair(13, 5,  "muscle");
+    s.addPair(6, 3,  "muscle");
+    s.addPair(6, 8,  "muscle");
+    s.addPair(4, 2,  "muscle");
+    s.addPair(4, 8,  "muscle");
+    s.addPair(7, 10,  "muscle");
+    s.addPair(5, 10,  "muscle");
+    s.addPair(3, 10,  "muscle");
+    s.addPair(3, 8,  "muscle");
+    s.addPair(2, 10,  "muscle");
+    s.addPair(2, 8,  "muscle");
     s.addPair(13, 7,  "muscle");
-    s.addPair(12, 5,  "muscle");
+    s.addPair(13, 5,  "muscle");
     s.addPair(12, 7,  "muscle");
+    s.addPair(12, 5,  "muscle");
+    s.addPair(7, 17,  "muscle");
+    s.addPair(7, 15,  "muscle");
+    s.addPair(5, 16,  "muscle");
+    s.addPair(5, 14,  "muscle");
     s.addPair(13, 15,  "muscle");
     s.addPair(13, 14,  "muscle");
     s.addPair(12, 17,  "muscle");
     s.addPair(12, 16,  "muscle");
+    s.addPair(10, 15,  "muscle");
+    s.addPair(10, 14,  "muscle");
+    s.addPair(11, 17,  "muscle");
+    s.addPair(11, 16,  "muscle");
 
-    // s.addPair(11, 14,  "muscle");
-    // s.addPair(11, 15,  "muscle");
-    s.addPair(11, 17,  "muscle"); //#
-    s.addPair(11, 16,  "muscle"); //#
-    s.addPair(10, 16,  "muscle");
-    s.addPair(10, 17,  "muscle");
-
-
-    s.addPair(10, 15,  "muscle");   //#
-    s.addPair(10, 14,  "muscle");   //#
-
-    s.addPair(14, 5,  "muscle");
-    s.addPair(15, 7,  "muscle");
-    s.addPair(16, 5,  "muscle");
-    s.addPair(17, 7,  "muscle");
 
     //?
     //?
@@ -226,8 +277,9 @@ void legzModel::setup(tgWorld& world)
     // Define the configurations of the rods and strings
     // Note that pretension is defined for this string
     const tgRod::Config rodConfig(c.radius, c.density);
-    const tgSpringCableActuator::Config muscleConfig(c.stiffness, c.damping, c.pretension);
-    
+    // const tgSpringCableActuator::Config muscleConfig(c.stiffness, c.damping, c.pretension);
+     const tgBasicActuator::Config muscleConfig(c.stiffness, c.damping, c.pretension,
+        c.hist, c.maxTension);
     // Create a structure that will hold the details of this model
     tgStructure s;
     
