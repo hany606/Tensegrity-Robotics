31bb0fa143fcd28c49b8fc6d74fd938633cc3a33
Hany Hamed <h.hamed.elanwar@gmail.com>
2019-12-24 14:15:53 +0300

    [Bugs fixing] Fix silly probllem in the name of the setup.sh file in the Dockerfile

--- bin/setup_helper.sh
+++ bin/setup_helper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dir=$(dirname $0)/..
 
-bash $dir/build.sh
-bash $dir/build.sh
-bash $dir/build.sh
+bash $dir/setup.sh
+bash $dir/setup.sh
+bash $dir/setup.sh
 
 bash $dir/bin/setup_gym_env.sh
